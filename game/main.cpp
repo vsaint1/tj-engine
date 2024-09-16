a15bafbc4b5ba86142b10c2922c6b562daf127df
vsaint1 <vsantos1.dev@gmail.com>
2024-09-16 00:32:35 -0300

    rm: unused file

--- game/main.cpp
+++ game/main.cpp
@@ -42,7 +42,7 @@ int main() {
 
     while (window.isOpen()) {
         sf::Event event{};
-        
+
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
