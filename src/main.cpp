7e0a449d02b81d5a9852c6789812380e3748ce05
vsaint1 <vsantos1.dev@gmail.com>
2024-10-14 00:26:05 -0300

    test renamed obj

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,10 @@ int main() {
     tj::Engine engine("TJ - FW <0.0.1>");
 
     auto playerObj = std::make_shared<GameObject>("player");
-    auto playerObj2 = std::make_shared<GameObject>("player");
+    auto entityObj = std::make_shared<GameObject>("entity");
 
     engine.AddGameObject(playerObj);
-    engine.AddGameObject(playerObj2);
+    engine.AddGameObject(entityObj);
 
     engine.Run();
 
