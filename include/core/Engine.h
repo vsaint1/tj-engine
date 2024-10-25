642635b1aa51be8c15d4d77b229a17b3bb36a638
vsaint1 <vsantos1.dev@gmail.com>
2024-10-25 10:37:49 -0300

    chore: engine instance

--- include/core/Engine.h
+++ include/core/Engine.h
@@ -12,7 +12,13 @@ namespace tj {
 
     class TJ_API Engine {
     public:
-        Engine(const std::string& _title, bool _bVsync = true, sf::Uint8 _fps = 60);
+
+        static Engine& GetInstance() {
+            static Engine instance;
+            return instance;
+        }
+
+        bool Create(const std::string& _title, bool _bVsync = true, sf::Uint8 _fps = 60);
 
         void Start();
 
@@ -48,13 +54,29 @@ namespace tj {
         void ChangeScene(const std::string& _newScene) {
             this->sceneManager.ChangeScene(_newScene, true);
             this->Start();
+        }
 
+        template <typename T>
+        std::shared_ptr<T> GetGameObject(const std::string& _name) {
+            auto sceneName = this->sceneManager.GetActiveScene();
+
+            auto gameObjects = this->sceneManager.GetScenes()[sceneName]->GetGameObjects();
+
+            for (auto [id, gObject] : gameObjects) {
+                if (gObject->GetName() == _name) {
+                    return std::dynamic_pointer_cast<T>(gObject);
+                }
+            }
+
+            TJ_LOG_ERROR("Couldn't find object with name: %s", _name.c_str());
+
+            return nullptr;
         }
 
     private:
-        Engine(Engine const& other)            = delete;
-        Engine& operator=(Engine const& other) = delete;
+        Engine() = default;
 
+        static tj::Engine* instance;
         bool bVsync;
         sf::Uint8 frameRate;
         std::unique_ptr<sf::RenderWindow> window;
